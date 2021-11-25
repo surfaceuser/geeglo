33ff89a2ba767de8383ec6bc515f039ba4f94d4b
cheems <82317146+surfaceuser@users.noreply.github.com>
2021-11-25 11:42:46 +0700

    fixcode

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-var input = document.getElementById("searchbox");
-input.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("search").click();
-    }
-});
 function search() {
   var search1 = document.getElementById("searchbox")
   var search = search1.value
